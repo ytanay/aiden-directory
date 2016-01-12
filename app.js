5e2c230e6edc5b9ad41783df95ea5282a1ceb818
Yotam Tanay <yotam@paticon.com>
2016-01-12 23:23:35 +0200

    Commit 2

--- app.js
+++ app.js
@@ -1,29 +1,55 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var uuid = require('node-uuid');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var _ = require('lodash');
 
 var app = express();
+var nodes = {};
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.get('/', function(req, res){
+  return res.send('Welcome to erans-directory, a component of ERANS, on ' + require('os').hostname())
+})
+
+app.get('/nodes', function(req, res){
+  return res.send(`
+    <pre>${JSON.stringify(nodes, null, 4)}</pre>
+    <script>setTimeout(function(){location.reload()}, 500);</script>
+  `);
+})
+
+app.post('/join', function(req, res){
+  if(!req.body.port)
+    throw 'Missing port'
+  
+  var address = '[' + req.ip + ']:' + req.body.port;
+  console.log('Welcoming new node', address, req.body);
+
+  res.json({
+    self: address,
+    nodes: nodes
+  });
+  
+  nodes[address] = {
+    
+    key: req.body.publicKey
+  };
+
+})
+
+app.get('/list', function(req, res){
+  return res.json({
+    nodes: nodes
+  })
+});
+
+app['delete']('/node/:address', function(req, res){
+  console.log('Removing node', req.params.address)
+  delete nodes[req.params.address]
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,14 +58,13 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.json({
+      error: true,
       message: err.message,
       error: err
     });
@@ -50,11 +75,13 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json({
+    error: true,
     message: err.message,
     error: {}
   });
 });
 
+app.listen(process.env['PORT'] || 5000)
 
 module.exports = app;
