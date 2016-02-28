bb39c7ef25032cf3f00ede66129f5fe3b11fcb5a
Yotam Tanay <yotam@paticon.com>
2016-02-28 22:39:22 +0200

    Just can't get over that

--- app.js
+++ app.js
@@ -5,24 +5,30 @@ var _ = require('lodash');
 var app = express();
 var nodes = {};
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
-  return res.send('Welcome to erans-directory, a component of ERANS, on ' + require('os').hostname())
+  return res.sendFile(__dirname + '/views/index.html');
 })
 
 app.get('/nodes', function(req, res){
-  return res.send(`
-    <pre>${JSON.stringify(nodes, null, 4)}</pre>
-    <script>setTimeout(function(){location.reload()}, 500);</script>
-  `);
+  return res.json(nodes);
 })
+
+app.post('/event/:name', function(req, res){
+  io.emit(req.params.name, req.body);
+  nodes[req.body.source][req.body.increment]++;
+  return res.send();
+});
 
 app.post('/join', function(req, res){
   if(!req.body.port)
     throw 'Missing port'
-  
+
   var address = '[' + req.ip + ']:' + req.body.port;
   console.log('Welcoming new node', address, req.body);
 
@@ -30,11 +36,19 @@ app.post('/join', function(req, res){
     self: address,
     nodes: nodes
   });
-  
-  nodes[address] = {
-    
-    key: req.body.publicKey
-  };
+
+  var nodeState = {
+    key: req.body.publicKey,
+    requests: 0,
+    carries: 0
+  }
+
+  nodes[address] = nodeState;
+
+  io.emit('node join', {
+    address: address,
+    state: nodeState
+  });
 
 })
 
@@ -56,32 +70,17 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      error: true,
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     error: true,
     message: err.message,
-    error: {}
+    error: err
   });
 });
 
 var PORT = process.env['PORT'] || 9000;
-app.listen(PORT, function(){
+http.listen(PORT, function(){
   console.log('erans-directory: Bound succesfully to port', PORT);
 })
 
